122ce96d3d0bfd02cf6582a8b9d251fac19fe207
stevebiczyk <stevebiczyk@gmail.com>
2023-05-24 18:27:34 +0000

    Rendering Lists Challenge completed including destructuring

--- my-app/src/components/PostItem.js
+++ my-app/src/components/PostItem.js
@@ -2,14 +2,17 @@ import React from 'react'
 import css from './css/PostItem.module.css'
 
 function PostItem(props) {
-    return props.savedPosts.map(post => (
-        <div key={post.id} className={css.SearchItem}>
-            <p>Title: {post.title}</p>
-            <p>Artist: {post.name}</p>
-            <img src={post.image} />
-            <p>Description: {post.description}</p>
-        </div>
-    ));
+    return props.savedPosts.map(post => {
+        const { id, title, name, image, description } = post;
+        return (
+            <div className={css.SearchItem} key={title}>
+                <p>Title: {title}</p>
+                <p>Artist: {name}</p>
+                <img src={image} alt={title} />
+                <p>Description: {description}</p>
+            </div>
+        );
+    });
 }
 
 export default PostItem;
