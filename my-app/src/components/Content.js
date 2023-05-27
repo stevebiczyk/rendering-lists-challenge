e793fe523f37b3c8280d8863555d7fd6572461f4
stevebiczyk <stevebiczyk@gmail.com>
2023-05-27 20:19:57 +0000

    Component Lifecyles Challenge solution

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -2,32 +2,40 @@ import React, { Component } from 'react';
 import css from "./css/Content.module.css";
 import { savedPosts } from '../posts.json';
 import PostItem from './PostItem';
+import Loader from './Loader';
 
 class Content extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            isLoaded: false,
+        };
+    }
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({
+                isLoaded: true,
+            });
+        }, 2000);
     }
 
     render() {
         return (
             <div className={css.Content}>
+
                 <div className={css.TitleBar}>
-                    <h1>My Posts</h1>
+                    <h1>My Photos</h1>
                 </div>
 
                 <div className={css.SearchResults}>
-                    {savedPosts.map(post => (
-                        <div key={post.id} className={css.SearchItem}>
+                    {
+                        this.state.isLoaded ?
                             <PostItem savedPosts={savedPosts} />
-                            {/* <p>Title: {post.title}</p>
-                            <p>Artist: {post.name}</p>
-                            <img src={post.image} />
-                            <p>Description: {post.description}</p> */}
-                        </div>
-                    ))}
+                            : <Loader />
+                    }
                 </div>
             </div>
-        );
+        )
     }
 }
 
